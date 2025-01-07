2a064bf2b68514d42ab469cf13ed17ad73713050
Manjunatha Nagaraja <nmanjunatha@gmail.com>
2025-01-07 01:06:25 +0000

    added more permission1

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,7 +13,7 @@ fi
 export GOTMPDIR="$JENKINS_HOME/$ENV"
 mkdir -p $GOTMPDIR
 echo "*** Building the app ..."
-sudo chmod -R 777 /tmp/cgo-gcc-input-2606243474
-sudo chmod -R 777 /usr/local/go
+chmod -R 777 /tmp/cgo-gcc-input-2606243474
+chmod -R 777 /usr/local/go
 
 go build -o $GOTMPDIR/$ENV main.go
